2615635f587c53c9921318788ab861973bb4e6d5
Glory2Ukraine <vorobiova.yu@gmail.com>
2023-09-03 05:50:55 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Yulia Vorobiova and is{" "}
+        <a
+          href="https://github.com/Glory2Ukraine/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
