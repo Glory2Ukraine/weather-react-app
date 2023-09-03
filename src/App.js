420856a566776e35f930ee530c90de443f2444c2
Glory2Ukraine <vorobiova.yu@gmail.com>
2023-09-03 06:29:33 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Yulia Vorobiova and is{" "}
-        <a
-          href="https://github.com/Glory2Ukraine/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Yulia Vorobiova and is{" "}
+          <a
+            href="https://github.com/Glory2Ukraine/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
