cca36516647470430df6890d0ebbefdbab0efbe2
Glory2Ukraine <vorobiova.yu@gmail.com>
2023-09-08 05:13:17 +0100

    Added a link to Netlify

--- src/App.js
+++ src/App.js
@@ -12,9 +12,17 @@ export default function App() {
           <a
             href="https://github.com/Glory2Ukraine/weather-react-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weather-app-by-jv.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
